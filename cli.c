dda49c3c5933c3269e71110c774299ed0856f28f
cn123-45 <cin.thitirath@gmail.com>
2026-04-22 15:19:47 +0000

    add initTaskBacklog function

---
+++ cli.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+
+void initTaskBacklog() {
+    int choice = 0;
+    int n;
+
+    printf("\n========================================\n");
+    printf("   SYSTEM INITIALIZATION: TASK BACKLOG\n");
+    printf("========================================\n");
+    
+    while (choice != 2) {
+        printf("\n1. Enqueue a new task\n");
+        printf("2. Finish adding tasks and proceed\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        if (choice == 1) {
+            printf("\n>> Enqueuing new task...\n");
+            scanf("%d", &n); //might change
+            enqueueTask(n);  //might change
+        } else if (choice != 2) {
+            printf("\n[!] Invalid choice. Please press 1 or 2.\n");
+        }
+    }
+}
+
+
+int main(){
+
+    return 0;
+}
