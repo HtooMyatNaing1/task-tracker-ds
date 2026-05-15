bfc429f3f2885968e5d19eaacf5d9c2ac752f1ff
HtooMyatNaing1 <htoomyatn396@gmail.com>
2026-05-15 17:12:24 +0700

    Final Benchmark

--- benchmark/benchmark.c
+++ benchmark/benchmark.c
@@ -7,6 +7,8 @@
 #include "../include/linear.h"
 
 #define TEST_SIZE 10000
+// Number of repeated searches to average timing results
+#define REPEATS 1000
 
 void generateName(char *buffer, int index) {
     sprintf(buffer, "Member_%d", index);
@@ -19,37 +21,64 @@ int main() {
     double bst_time, linear_time;
 
     printf("=== BENCHMARK TEST ===\n");
-    printf("Inserting %d members...\n\n", TEST_SIZE);
+    printf("Inserting %d members (randomized)...\n\n", TEST_SIZE);
+
+    // Create randomized insertion order to avoid degenerate BST
+    int *indices = (int *)malloc(sizeof(int) * TEST_SIZE);
+    if (!indices) {
+        fprintf(stderr, "Failed to allocate indices array\n");
+        return 1;
+    }
+
+    for (int i = 0; i < TEST_SIZE; i++)
+        indices[i] = i;
+
+    srand((unsigned)time(NULL));
+    for (int i = TEST_SIZE - 1; i > 0; i--) {
+        int j = rand() % (i + 1);
+        int tmp = indices[i];
+        indices[i] = indices[j];
+        indices[j] = tmp;
+    }
 
     // ----------------------------
-    // Insert Members
+    // Insert Members (random order)
     // ----------------------------
-    for (int i = 0; i < TEST_SIZE; i++) {
+    for (int k = 0; k < TEST_SIZE; k++) {
+        int i = indices[k];
         generateName(name, i);
         insertMemberBST(name);
         insertMemberLinearly(name);
     }
 
+    free(indices);
+
     // Target to search (worst-case: last element)
     generateName(name, TEST_SIZE - 1);
 
     // ----------------------------
-    // BST SEARCH TIMING
+    // BST SEARCH TIMING (averaged)
     // ----------------------------
     start = clock();
-    Member* foundBST = searchMemberBST(name);
+    Member* foundBST = NULL;
+    for (int r = 0; r < REPEATS; r++) {
+        foundBST = searchMemberBST(name);
+    }
     end = clock();
 
-    bst_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    bst_time = (((double)(end - start)) / CLOCKS_PER_SEC) / REPEATS;
 
     // ----------------------------
-    // LINEAR SEARCH TIMING
+    // LINEAR SEARCH TIMING (averaged)
     // ----------------------------
     start = clock();
-    int foundLinear = searchMemberLinearly(name);
+    int foundLinear = -1;
+    for (int r = 0; r < REPEATS; r++) {
+        foundLinear = searchMemberLinearly(name);
+    }
     end = clock();
 
-    linear_time = ((double)(end - start)) / CLOCKS_PER_SEC;
+    linear_time = (((double)(end - start)) / CLOCKS_PER_SEC) / REPEATS;
 
     // ----------------------------
     // RESULTS
